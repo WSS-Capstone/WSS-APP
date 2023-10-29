ea48ce5a3be8d417a9fd9dc79c7d9a20d2674b4a
lnong <lnong@rosen-group.com>
2023-10-29 21:35:39 +0700

    fix get list tasks

--- src/app/modules/admin/apps/task/task.types.ts
+++ src/app/modules/admin/apps/task/task.types.ts
@@ -12,8 +12,8 @@ export interface Task {
     comments: string[];
     partner?: User;
     staff?: User;
-    service: Service;
-    order: Order;
+    service?: Service;
+    order?: Order;
 }
 
 export interface TaskPagination {
